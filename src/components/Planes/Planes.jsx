91528c5bf19d03612a69dc91e354b6e2476626ed
Dominguez Bonini Andrés Santiago <andres.dominguez@novaseguroslatam.com>
2021-11-14 18:04:29 -0500

    Added a Planes Component

---
+++ src/components/Planes/Planes.jsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import  {Box, Grid} from '@mui/material';
+
+
+export default function Planes() {
+
+    return (
+
+        <section>
+ <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+    <p>Hello World</p>
+        </Grid>
+        <Grid item xs={4}>
+        <p>Hello World</p>
+        </Grid>
+        <Grid item xs={4}>
+        <p>Hello World</p>
+        </Grid>
+        <Grid item xs={8}>
+        <p>Hello World</p>
+        </Grid>
+      </Grid>
+    </Box>
+</section>
+
+    );
+
+}
