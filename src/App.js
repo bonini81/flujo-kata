f8fe17a0f965dac87d1058f54ab201d2da2d9a88
Dominguez Bonini Andrés Santiago <andres.dominguez@novaseguroslatam.com>
2021-11-14 21:02:27 -0500

    React Webcam Install Package

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Planes from './components/Planes/Planes';
+import Inspeccion from './components/Inspeccion/Inspeccion';
+
 
 function App() {
   return (
     <div className="App">
      <Header />
      <Planes />
+     <Inspeccion /> 
+
     </div>
   );
 }
---
+++ src/components/Inspeccion/Inspeccion.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Webcam from "react-webcam";
+
+export default function  WebcamCapture() {
+    const webcamRef = React.useRef(null);
+    const [imgSrc, setImgSrc] = React.useState(null);
+  
+    const capture = React.useCallback(() => {
+      const imageSrc = webcamRef.current.getScreenshot();
+      setImgSrc(imageSrc);
+    }, [webcamRef, setImgSrc]);
+  
+    return (
+      <>
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+        />
+        <button onClick={capture}>Capture photo</button>
+        {imgSrc && (
+          <img
+            src={imgSrc}
+          />
+        )}
+      </>
+    );
+  };
+  
+
+  // https://www.npmjs.com/package/react-webcam
+  
